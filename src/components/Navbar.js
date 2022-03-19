df32484c29b7ef2c668117bf0a839191e4b873bb
Ahmad Elshowair <it.is.work@outlook.com>
2022-03-19 15:28:33 +0700

    eliminate the header element and re-style the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,11 @@
-import airbnb_image from '../images/airbnb.png'
+import airbnb_image from '../images/airbnb-logo.png'
 
 export const Navbar = ()=>{
     return (
-        <header className='header'>
-            <nav className='nav'>
-                <div className='nav__logo'>
-                    <img src={airbnb_image} alt='airbnb logo' className='nav__logo-image'/>
-                </div>
-            </nav>
-        </header>
-
+        <nav className='nav'>
+            <div className='nav__logo'>
+                <img src={airbnb_image} alt='airbnb logo' className='nav__logo-image'/>
+            </div>
+        </nav>
     );
 };
