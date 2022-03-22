08e4b12906ef9fc7f47a44b1239509c8be65c300
Ahmad Elshowair <it.is.work@outlook.com>
2022-03-22 12:47:19 +0700

    use porps with the component ccard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { Card } from "./components/Card";
 import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
+import katie from './images/katie-zaferes.png';
 
 function App() {
   return (
     <div className="container">
      <Navbar />
      <Hero />
-     <Card />
+     <Card 
+      image={katie}
+      rating="5.0"
+      reviewCount="6"
+      country="USA"
+      title="Life lessons with Katie Zaferes"
+      price="136"
+     />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,21 @@
-import katie from '../images/katie-zaferes.png';
 import star from '../images/star.png';
-export const Card = () =>{
+
+export const Card = (props) =>{
+    console.log(props)
     return (
         <div className='card'>
             <div className="card__image">
                 <span className='card__btn'>sold out</span>
-                <img src={katie} alt="katie zaferes"className="image"/>
+                <img src={props.image} alt="katie zaferes" className="image"/>
             </div>
             <div className="card__rate">
                 <img src={star} alt="star" className="card__rate-image m_l_4"/>
-                <span className='m_l_4'>5.0</span>
-                <span  className=' m_l_4 gray'>(6) • </span>
-                <span className='gray'>USA</span>
+                <span className='m_l_4'>{props.rating}</span>
+                <span  className=' m_l_4 gray'>({props.reviewCount}) • </span>
+                <span className='gray'>{props.country}</span>
             </div>
-            <p className="card__text">Life lessons with Katie Zaferes</p>
-            <p className="price"><span className='bold'>From $136</span> / person</p>
+            <p className="card__text">{props.title}</p>
+            <p className="price"><span className='bold'>From ${props.price}</span> / person</p>
         </div>
     );
 };
